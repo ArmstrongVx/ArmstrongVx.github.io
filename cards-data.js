02aae32e4ab393bd377f70e00771d8f06ec6803c
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-11-18 12:50:55 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/Sticky Sorcerer/index.html', imgSrc: 'sticky sorcerer.png', name: 'Sticky Sorcerer' , 'page': 5, 'id': 979 },
   { href: './html/Supermarioplay/index.html', imgSrc: 'supermarioplay.jpeg', name: 'Supermarioplay' , 'page': 5, 'id': 981 },
   { href: './html/Survival Race/index.html', imgSrc: 'survival race.jpeg', name: 'Survival Race' , 'page': 5, 'id': 982 },
   { href: './html/Swords and sandals 2/index.html', imgSrc: 'swords and sandals 2.jpeg', name: 'Swords and sandals 2' , 'page': 5, 'id': 983 },
